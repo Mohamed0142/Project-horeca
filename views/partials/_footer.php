a387c3e77e2eda1c7ed0b08d0653cfc4922db2bd
mo121213132131 <hamoudishaaban16@gmail.com>
2024-04-22 12:57:44 +0200

    footer & nav

---
+++ views/partials/_footer.php
@@ -0,0 +1,75 @@
+  
+    <footer>
+        <div class="list">
+          <div class="footer-block">
+            <h2>Contact</h2>
+            <ul>
+              <li><a href="">lorem</a></li>
+              <br />
+              <li><a href="">lorem</a></li>
+              <br />
+              <li><a href="">lorem</a></li>
+            </ul>
+          </div>
+          <div class="footer-block">
+            <h2>Info</h2>
+            <ul>
+              <li><a href="">lorem</a></li>
+              <br />
+              <li><a href="">lorem</a></li>
+              <br />
+              <li><a href="">lorem</a></li>
+            </ul>
+          </div>
+        </div>
+  
+        <div class="right-side">
+          <h2>Socials</h2>
+          <ul class="socials">
+            <li>
+              <a href="https://www.facebook.com"
+                ><i class="fa fa-facebook"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li>
+              <a href="https://plus.google.com"
+                ><i class="fa fa-google-plus"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com"><i class="fa fa-youtube"></i></a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com"
+                ><i class="fa fa-linkedin-square"></i
+              ></a>
+            </li>
+            <li>
+              <a href="https://web.whatsapp.com/"
+                ><i class="fa-brands fa-whatsapp"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+  
+        <div class="formulier">
+          <div class="formulier-box">
+             <h2>Contact ons</h2>
+           
+            <form method='post'>
+              <div class="user-box-naam">
+                <input type="text" name="name"  placeholder="Email" />
+              </div>
+              <div class="user-box-feedback">
+                <input type="text" name="email"  placeholder="Uw bericht" />
+              </div>
+              <a href="#">
+                <button type="submit" class="Submit-button">Verzenden</button>
+              </a>
+            </form>
+          </div>
+        </div>
+      </footer>
---
+++ views/partials/_nav.php
@@ -0,0 +1,16 @@
+<header>
+        <nav>
+            <ul class='nav-bar'>
+                <li class='logo'><a href='/'><img src='/img/logo-nav.png'/></a></li>
+                <input type='checkbox' id='check' />
+                <span class="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/info">Info</a></li>
+                    <li><a href="/testjezelf">Test Jezelf</a></li>
+                    <li><a href="/contact">Contact Ons</a></li>
+                    <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+                </span>
+                <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
+            </ul>
+        </nav>
+      </header>
