744ac505280eabee5dc0fc0af27e7dd8a0a611c7
mo121213132131 <hamoudishaaban16@gmail.com>
2024-04-26 09:15:37 +0200

    update nav

--- core/router.php
+++ core/router.php
@@ -25,21 +25,6 @@ class Router
         $this->add('POST', $uri, $controller);
     }
 
-    public function delete($uri, $controller)
-    {
-        $this->add('DELETE', $uri, $controller);
-    }
-
-    public function patch($uri, $controller)
-    {
-        $this->add('PATCH', $uri, $controller);
-    }
-
-    public function put($uri, $controller)
-    {
-        $this->add('PUT', $uri, $controller);
-    }
-
     public function route($uri, $method)
     {
         foreach ($this->routes as $route) {
--- views/index.php
+++ views/index.php
@@ -1,8 +1,51 @@
-<?php require_once __DIR__ . '/partials/_header.php' ?>
-
+<?php require_once __DIR__ . '/partials/_header.php'; ?>
 
 <body>
+<img src="img/Home.png" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-s-12">
+                
+                <div class="text-overlay">
+                    <h2>Horeca opleiding</h2>
+                    <button class="button-overlay">Read More</button>
+                    <button class="button-overlay">Contact Us</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-s-4 box-home">
+                <img src="img/Home-contact.png" alt="">
+                <div class="text-box">
+                    <p>
+                        <strong>Contact ons</strong> <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ip consectetur adipiscing elit. <br>
+                        <button class="button-home">contact ons</button>
+                    </p>
+                </div>
+            </div>
 
+            <div class="col-s-4 box-home">
+                <img src="img/Home-Inschrijf.png" alt="">
+                <div class="text-box">
+                    <p>
+                        <strong>Inschrijven</strong> <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ip consectetur adipiscing elit. <br>
+                        <button class="button-home">Schrijf je in!</button>
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-s-4 box-home">
+                <img src="img/Home-read-more.png" alt="">
+                <div class="text-box">
+                    <p>
+                        <strong>Opleiding</strong> <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Lorem ip consectetur adipiscing elit. <br>
+                        <button class="button-home">Lees meer</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 
-<?php require_once __DIR__ . '/partials/_footer.php' ?>
+<?php require_once __DIR__ . '/partials/_footer.php'; ?>
