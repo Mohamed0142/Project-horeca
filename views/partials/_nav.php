c6ee15a4b2f14c6eb3c1b3faff40e1a4cdb41fd4
mo121213132131 <hamoudishaaban16@gmail.com>
2024-05-27 12:48:56 +0200

    a

--- views/partials/_nav.php
+++ views/partials/_nav.php
@@ -5,7 +5,7 @@
                 <input type='checkbox' id='check' />
                 <span class="menu">
                     <li><a href="/">Home</a></li>
-                    <li><a href="/info">Info</a></li>
+                    <li><a href="/about">Info</a></li>
                     <li><a href="/inschrijf">Inschrijven</a></li>
                     <li><a href="/contact">Contact Ons</a></li>
                     <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
