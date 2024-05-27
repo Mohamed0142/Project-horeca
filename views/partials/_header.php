21c9fd2e7d4a802ce61b803d4b2c4a3b70c6c614
SalihEris <335385@student.mboutrecht.nl>
2024-05-27 12:53:05 +0200

    header

--- views/partials/_header.php
+++ views/partials/_header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/inschrijf.css">
+    <?php if($_SERVER['REQUEST_URI'] === '/'): ?><link rel="stylesheet" href="/css/index.css"> <?php endif; ?>
+    <?php if($_SERVER['REQUEST_URI'] === '/inschrijf'): ?><link rel="stylesheet" href="/css/inschrijf.css"> <?php endif; ?>
     <script src="https://kit.fontawesome.com/06a2eca3f5.js" crossorigin="anonymous"></script>
 </head>
 
